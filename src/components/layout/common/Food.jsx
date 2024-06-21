c49d2ac6846349489f572719a572e5a806a18b67
Daniel Lopez <dast11lp@gmail.com>
2024-06-20 23:38:08 -0500

    change way to get static images

--- src/components/layout/common/Food.jsx
+++ src/components/layout/common/Food.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
-import { Card } from './Card'
 import CardFood from './CardFood'
+
+import burgerImage from '../../../assets/img/burger_freesh_drink.jpg';
+import hotdogImage from '../../../assets/img/hotdog.jpg';
+import sodaImage from '../../../assets/img/soda.jpg';
+
 
 export const Food = () => {
   const foods = [
@@ -8,19 +12,19 @@ export const Food = () => {
         id: 1,
         name: "Combo",
         description: "Un combo clásico que combina una refrescante soda, crujientes papas fritas y una jugosa hamburguesa. Ideal para satisfacer el hambre con sabores deliciosos y familiares.",
-        img: "./src/assets/img/burger_freesh_drink.jpg"
+        img: burgerImage,
     },
     {
         id: 2,
         name: "Perro caliente",
         description: "Un perro caliente jugoso y sabroso, envuelto en un pan suave y esponjoso, acompañado de salsas y toppings que resaltan su sabor único. Un clásico irresistible que combina texturas crujientes y cremosas en cada bocado.",
-        img: "./src/assets/img/hotdog.jpg"
+        img: hotdogImage
     },
     {
         id: 3,
         name: "Gaseosa",
         description: "Un refresco burbujeante y refrescante que sacia la sed al instante, con su característico sabor dulce y una sensación ligera en el paladar. Perfecto para acompañar cualquier comida o disfrutar por sí solo en cualquier momento del día.",
-        img: "./src/assets/img/soda.jpg"
+        img: hotdogImage
     }]
   return (
     <div>
