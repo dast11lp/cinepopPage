d154cee1f5e8ede17cc31e442a6bd7b036e5af9b
Daniel Lopez <dast11lp@gmail.com>
2024-08-30 13:52:11 -0500

    render.com api added

--- src/app/config.jsx
+++ src/app/config.jsx
@@ -1,6 +1,7 @@
 const Config = {
     // hostname: 'http://localhost:8080/',
-    hostname: 'https://cinema-sprinb-boot-production.up.railway.app/'
+    // hostname: 'https://cinema-sprinb-boot-production.up.railway.app/'
+    hostname: 'https://cinepopserver.onrender.com'
 }
 
 export {Config}
